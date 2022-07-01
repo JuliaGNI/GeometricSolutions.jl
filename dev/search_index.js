8b66dfeb1332d5ac39742ac6c8155a7a5466c6e3
Documenter.jl <documenter@juliadocs.github.io>
2022-07-01 11:47:37 +0000

    build based on 8edc1bd

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = GeometricSolutions","category":"page"},{"location":"#GeometricSolutions","page":"Home","title":"GeometricSolutions","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for GeometricSolutions.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [GeometricSolutions]","category":"page"}]
+}
