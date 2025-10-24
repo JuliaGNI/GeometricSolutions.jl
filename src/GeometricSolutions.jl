0d29d7c667c186b9e000768c006a84221646278e
Michael Kraus <michael.kraus@ipp.mpg.de>
2025-10-24 11:31:26 +0200

    Add Union types for GeometricSolution depending on problem type: SolutionODE, SolutionDAE, SolutionSDE, SolutionPODE, SolutionPDAE, SolutionPSDE

--- src/GeometricSolutions.jl
+++ src/GeometricSolutions.jl
@@ -17,27 +17,41 @@ export initialtime, finaltime, timespan
 export eachtimestep, timestep, timesteps
 export maximum_error, relative_maximum_error, relative_norm_error
 
-
 include("utils.jl")
-
 
 include("dataseries.jl")
 
 export DataSeries, ScalarDataSeries
 
-
 include("timeseries.jl")
 
 export TimeSeries
-
 
 include("geometric_solution.jl")
 
 export GeometricSolution
 
-
 include("ensemble_solution.jl")
 
 export EnsembleSolution, solution
+
+const SolutionODE{dType, tType, dsType, probType, perType} = GeometricSolution{
+    dType, tType, dsType, probType,
+    perType} where {probType <: Union{ODEProblem, SODEProblem, SubstepProblem}}
+const SolutionDAE{dType, tType, dsType, probType, perType} = GeometricSolution{
+    dType, tType, dsType, probType, perType} where {probType <: DAEProblem}
+const SolutionSDE{dType, tType, dsType, probType, perType} = GeometricSolution{
+    dType, tType, dsType, probType, perType} where {probType <: SDEProblem}
+const SolutionPODE{dType, tType, dsType, probType, perType} = GeometricSolution{
+    dType, tType, dsType, probType,
+    perType} where {probType <: Union{PODEProblem, HODEProblem, IODEProblem, LODEProblem}}
+const SolutionPDAE{dType, tType, dsType, probType, perType} = GeometricSolution{
+    dType, tType, dsType, probType,
+    perType} where {probType <: Union{PDAEProblem, HDAEProblem, IDAEProblem, LDAEProblem}}
+const SolutionPSDE{dType, tType, dsType, probType, perType} = GeometricSolution{
+    dType, tType, dsType, probType, perType} where {probType <: PSDEProblem}
+
+export SolutionODE, SolutionDAE, SolutionSDE
+export SolutionPODE, SolutionPDAE, SolutionPSDE
 
 end
