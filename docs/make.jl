JuliaGNI/GeometricOptimizers.jl

Format with JuliaFormatter
Mechanical reformat against this repository's own .JuliaFormatter.toml
(style = "sciml"). Whitespace, line breaks and bracket placement only;
no semantic change.

Tree-wide, 1486 of 1674 tracked .jl files did not satisfy the style
they already declare. That is why this is a separate commit per
repository: it is reviewable as "formatting only", or not reviewable
at all.

Scope: tracked files only, excluding prototyping/, legacy/ and
obsolete/. Untracked work is untouched.

Two files tree-wide are excluded because JuliaFormatter cannot process
them -- it hard-crashes the Julia process, rather than raising a
catchable error, on pathologically long lines:

    MultiSymplectic/src/utils/common.jl                1624-char line
    SparseIdentification/src/hamiltonianGenerator.jl   2108-char line

Both hold numerical tables written as long array literals. They will
block the new pre-commit hook if they are ever staged. Breaking those
literals across lines is a content change and is deliberately not made
here.

Verified after formatting: GeometricBase, GeometricSolutions and
QuadratureRules suites pass; RungeKutta loads and computes correctly.

Co-Authored-By: Claude Opus 5 <noreply@anthropic.com>

docs/make.jl
@@ -33,7 +33,7 @@ links = InterLinks(
         "https://juliagni.github.io/GeometricMachineLearning.jl/latest",
         "https://juliagni.github.io/GeometricMachineLearning.jl/latest/objects.inv",
         joinpath(@__DIR__, "inventories", "GeometricMachineLearning.toml")
-    ),
+    )
 )
 
 # The chapters that moved here from `GeometricMachineLearning` state their definitions, theorems and
@@ -58,11 +58,13 @@ function theorem(statement::String, name::String; label::Union{Nothing, String} 
             \t $(statement)""")
     else
         theorem_label = isnothing(label) ? "" : raw"\label{th:" * label * raw"}"
-        Markdown.parse(raw"\begin{thrm}[" * name * "]" * statement * theorem_label * raw"\end{thrm}")
+        Markdown.parse(raw"\begin{thrm}[" * name * "]" * statement * theorem_label *
+                       raw"\end{thrm}")
     end
 end
 
-function theorem(statement::String; name::Union{Nothing, String} = nothing, label::Union{Nothing, String} = nothing)
+function theorem(statement::String; name::Union{Nothing, String} = nothing,
+        label::Union{Nothing, String} = nothing)
     theorem(statement, name; label = label)
 end
 
@@ -123,21 +125,21 @@ const indentation = output_type == :html ? "\t" : ""
 bib = CitationBibliography(joinpath(@__DIR__, "src", "GeometricOptimizers.bib"))
 Bibliography.sort_bibliography!(bib.entries, :nyt)  # name-year-title
 
-DocMeta.setdocmeta!(GeometricOptimizers, :DocTestSetup, :(using GeometricOptimizers); recursive=true)
+DocMeta.setdocmeta!(GeometricOptimizers, :DocTestSetup, :(using GeometricOptimizers); recursive = true)
 
 makedocs(;
-    plugins=[bib, links],
-    modules=[GeometricOptimizers],
-    authors="Michael Kraus",
-    repo="https://github.com/JuliaGNI/GeometricOptimizers.jl/blob/{commit}{path}#{line}",
-    sitename="GeometricOptimizers.jl",
-    format=Documenter.HTML(;
-        prettyurls=get(ENV, "CI", "false") == "true",
-        canonical="https://JuliaGNI.github.io/GeometricOptimizers.jl",
-        edit_link="main",
+    plugins = [bib, links],
+    modules = [GeometricOptimizers],
+    authors = "Michael Kraus",
+    repo = "https://github.com/JuliaGNI/GeometricOptimizers.jl/blob/{commit}{path}#{line}",
+    sitename = "GeometricOptimizers.jl",
+    format = Documenter.HTML(;
+        prettyurls = get(ENV, "CI", "false") == "true",
+        canonical = "https://JuliaGNI.github.io/GeometricOptimizers.jl",
+        edit_link = "main",
         # The moved chapters include each of their figures twice, once per theme; without this
         # stylesheet both variants render, stacked. See `docs/src/assets/extra_styles.css`.
-        assets=["assets/extra_styles.css"],
+        assets = ["assets/extra_styles.css"],
         # `api.md` is one catch-all `@autodocs` over the whole package, so it is large by design.
         # `size_threshold_warn` is left at its default: the warning it prints for that one page is
         # the reminder that the docstrings are still not distributed over the chapters that explain
@@ -149,9 +151,9 @@ makedocs(;
         # actually bounds it is continuing the migration that moved the manifold, matrix,
         # global-section and retraction docstrings onto the chapters that explain them. Until that
         # is done this number will need raising again, and the warning above says so on every build.
-        size_threshold=450 * 2^10,
+        size_threshold = 450 * 2^10
     ),
-    pages=[
+    pages = [
         "Home" => "index.md",
         # The `Manifolds`, `Special Matrices` and `Optimizers` chapters came over from
         # `GeometricMachineLearning` with the types they describe; see GeometricMachineLearning#239.
@@ -162,30 +164,30 @@ makedocs(;
             "General Theory on Manifolds" => "manifolds/manifolds.md",
             "Differential Equations and the EAU theorem" => "manifolds/existence_and_uniqueness_theorem.md",
             "Riemannian Manifolds" => "manifolds/riemannian_manifolds.md",
-            "Homogeneous Spaces" => "manifolds/homogeneous_spaces.md",
+            "Homogeneous Spaces" => "manifolds/homogeneous_spaces.md"
         ],
         "Special Matrices" => [
             "Symmetric, Skew-Symmetric and Triangular Matrices" => "special_matrices.md",
-            "Global Tangent Spaces" => "global_tangent_spaces.md",
+            "Global Tangent Spaces" => "global_tangent_spaces.md"
         ],
         "Optimizers" => [
             "Optimization on Homogeneous Spaces" => "manifold_optimizers.md",
             "Retractions" => "retractions.md",
             "Parallel Transport" => "parallel_transport.md",
-            "Optimizer Methods" => "optimizer_methods.md",
+            "Optimizer Methods" => "optimizer_methods.md"
         ],
         "Linesearch" => [
             "Linesearch" => "linesearch.md",
             "Linesearches on Manifolds" => "linesearch_on_manifolds.md",
-            "Weight Decay on Manifolds" => "weight_decay.md",
+            "Weight Decay on Manifolds" => "weight_decay.md"
         ],
         "References" => "references.md",
-        "API" => "api.md",
-    ],
+        "API" => "api.md"
+    ]
 )
 
 deploydocs(;
-    repo="github.com/JuliaGNI/GeometricOptimizers.jl",
-    devurl="latest",
-    devbranch="main",
+    repo = "github.com/JuliaGNI/GeometricOptimizers.jl",
+    devurl = "latest",
+    devbranch = "main"
 )

JuliaGNI/GeometricSolutions.jl

docs/make.jl
@@ -6,26 +6,26 @@ links = InterLinks(
     "GeometricEquations" => "https://JuliaGNI.github.io/GeometricEquations.jl/stable/",
 )
 
-DocMeta.setdocmeta!(GeometricSolutions, :DocTestSetup, :(using GeometricSolutions); recursive=true)
+DocMeta.setdocmeta!(GeometricSolutions, :DocTestSetup, :(using GeometricSolutions); recursive = true)
 
 makedocs(;
-    modules=[GeometricSolutions],
-    authors="Michael Kraus",
-    repo="https://github.com/JuliaGNI/GeometricSolutions.jl/blob/{commit}{path}#{line}",
-    sitename="GeometricSolutions.jl",
-    format=Documenter.HTML(;
-        prettyurls=get(ENV, "CI", "false") == "true",
-        canonical="https://JuliaGNI.github.io/GeometricSolutions.jl",
-        assets=String[],
+    modules = [GeometricSolutions],
+    authors = "Michael Kraus",
+    repo = "https://github.com/JuliaGNI/GeometricSolutions.jl/blob/{commit}{path}#{line}",
+    sitename = "GeometricSolutions.jl",
+    format = Documenter.HTML(;
+        prettyurls = get(ENV, "CI", "false") == "true",
+        canonical = "https://JuliaGNI.github.io/GeometricSolutions.jl",
+        assets = String[]
     ),
-    plugins=[links],
-    pages=[
+    plugins = [links],
+    pages = [
         "Home" => "index.md",
-    ],
+    ]
 )
 
 deploydocs(;
-    repo   = "github.com/JuliaGNI/GeometricSolutions.jl",
+    repo = "github.com/JuliaGNI/GeometricSolutions.jl",
     devurl = "latest",
-    devbranch = "main",
+    devbranch = "main"
 )